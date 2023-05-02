fa46388860243e78a9cf1ae815ed3dead6636d01
NICOLA-200 <fatepepe66@gmail.com>
2023-05-02 21:01:14 +0200

    added some styling

---
+++ script.js
@@ -0,0 +1,30 @@
+   const popup = document.querySelector(".popup");
+   
+   let isOnline = true;
+   const checkConnection = async () => {
+     try {
+
+          const response = await fetch ("https://jsonplaceholder.typicode.com/posts");
+          isOnline = response.status >= 200 && response.status < 300;
+          
+
+     } catch (err) {
+
+          isOnline = false;
+
+     }
+     handlePopup(isOnline);
+ {
+
+ }  
+
+}
+
+const handlePopup = (status) => {
+     if(status) {
+          return popup.classList.remove('show');
+     }
+     popup.classList.add("show")
+}
+
+setInterval(checkConnection, 3000);
